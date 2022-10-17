1cbbebdf4ffda9385368be4fc178c120adea4435
TermiSenpai <escarpaprieto@gmail.com>
2022-10-17 12:56:54 +0200

    PlayerMovement script

--- TokioPlatform2D/Assets/Scripts/Player/PlayerMovement.cs
+++ TokioPlatform2D/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [Space]
+    [Header("Properties")]
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpingPower;
+    private float horizontal;
+    private bool isFacingRight = true;
+
+
+
+    #region unity methods
+    private void Update()
     {
-        
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if (!isFacingRight && horizontal > 0f)
+            flip();
+        else if (isFacingRight && horizontal < 0f)
+            flip();
+    }
+    #endregion
+
+    #region private methods
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void flip()
     {
-        
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
     }
+
+    #endregion
+
+    #region inputActions
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if(context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    #endregion
 }
