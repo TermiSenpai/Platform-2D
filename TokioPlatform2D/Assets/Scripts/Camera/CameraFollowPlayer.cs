65e96ffc2ca12ae5b762dc692e974eae1e2e1c22
TermiSenpai <escarpaprieto@gmail.com>
2022-10-18 10:50:19 +0200

    Camera limits

--- TokioPlatform2D/Assets/Scripts/Camera/CameraFollowPlayer.cs
+++ TokioPlatform2D/Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float followSpeed;
+    [SerializeField] private float maxX;
+    [SerializeField] private float minX;
 
     private void FixedUpdate()
     {
@@ -13,5 +16,7 @@ public class CameraFollowPlayer : MonoBehaviour
         Vector3 newPos = new Vector3(target.position.x, transform.position.y, -10f) ;
         // translate the camera with a smooth movement
         transform.position = Vector3.Lerp(transform.position, newPos, followSpeed * Time.deltaTime);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), transform.position.y, transform.position.z);               
     }
 }
